8b8fb9abbe30dee91ccb84031389a48a3b795a09
bigdeal <bigdeal@smartstudy.co.kr>
2020-09-06 04:57:24 +0900

    SEO

--- nextjs/components/Loading.jsx
+++ nextjs/components/Loading.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { observer, inject } from 'mobx-react';
 import { withTranslation } from "react-i18next";
 import { Spin } from 'antd';
 
+@inject('user')
+@observer
 class Loading extends React.Component {
   render() {
-    const { i18n } = this.props;
+    const { i18n, user } = this.props;
+    const article = (user || {}).article || {};
+
     return (
       <div style={LoadingStyle}>
         <div style={LoadingTextStyle}>
           <Spin size="large" tip={`${i18n.t('loading')}...`} />
+
+          {/* SEO Optimization */}
+          <div style={{ display: 'none' }}>
+            <h1>{article.title}</h1>
+            <div className="article-content">{article.content}</div>
+            {
+              (article.comments || []).map((comment, index) => {
+                return <div key={index} className='article-comment'>
+                  {comment.content}
+                </div>
+              })
+            }
+          </div>
         </div>
       </div>
     )
