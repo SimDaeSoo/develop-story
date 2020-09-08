879e795a4be8e0923f088340ec26adcdf60d69b4
bigdeal <bigdeal@smartstudy.co.kr>
2020-09-08 14:35:44 +0900

    deploy 수정중.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+SCRIPT_MESSAGE='\033[1;33m'  # 1;33 is Yellow
+ALERT_MESSAGE='\033[1;31m'   # 1;31 is Light Red
+COMMAND_ECHO='\033[1;32m'    # 1;32 is Light Green
+NC='\033[0m' # No Color
+
+####
+# get current image_name
+if [ -f .image_name ]; then
+    IMAGE_NAME=`cat .image_name`
+else
+    echo "${ALERT_MESSAGE}There is not \'.image_name\' file.${NC}"
+    exit 1
+fi
 
 if [ -n "$CIRCLE_TAG" ]; then
     TAG_NAME="release-${CIRCLE_TAG}"
 else
     TAG_NAME="build-$(git rev-parse --short HEAD)"
+fi
 
-IMAGE_NAME="${DOCKER_IDENTIFIER}/${DOCKER_IMAGE_NAME}"
+echo "TAG_NAME is : ${TAG_NAME}"
+echo "DOCKER_IMAGE is ${DOCKER_IMAGE_NAME}"
 
+DOCKER_IMAGE_NAME="${DOCKER_IDENTIFIER}/${IMAGE_NAME}"
 docker login -u ${DOCKER_IDENTIFIER} -p ${DOCKER_PASSWORD}
-docker build -t ${IMAGE_NAME}:${TAG_NAME} .
-docker tag ${IMAGE_NAME}:${TAG_NAME} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}:${TAG_NAME}
+docker build -t ${DOCKER_IMAGE_NAME}:${TAG_NAME} .
+docker tag ${DOCKER_IMAGE_NAME}:${TAG_NAME} ${DOCKER_IMAGE_NAME}:latest
+docker push ${DOCKER_IMAGE_NAME}:latest
+docker push ${DOCKER_IMAGE_NAME}:${TAG_NAME}
+
+#!/bin/bash
+
+if [ -n "$CIRCLE_TAG" ]; then
+    TAG_NAME="release-${CIRCLE_TAG}"
+else
+    TAG_NAME="build-$(git rev-parse --short HEAD)"
